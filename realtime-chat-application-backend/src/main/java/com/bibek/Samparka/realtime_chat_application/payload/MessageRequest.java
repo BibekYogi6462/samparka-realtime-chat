bd932e78675c0791a921ef0375bf047c7f4bc2e5
bibekyogi6462 <bibekyogi6462@gmail.com>
2026-03-28 09:39:54 +0545

    Overall Backend Structured and Completed

---
+++ realtime-chat-application-backend/src/main/java/com/bibek/Samparka/realtime_chat_application/payload/MessageRequest.java
@@ -0,0 +1,20 @@
+package com.bibek.Samparka.realtime_chat_application.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequest {
+
+    private String content;
+    private String sender;
+    private String roomId;
+
+}
